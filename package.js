c2ad008be6ee154a3e26584802bc5e56dba24a95
Abigail Watson <awatson1978@gmail.com>
2016-02-06 21:10:02 -0800

    getPageHeight; getOpacity; etc

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name:'clinical:active-users',
-  version: '0.1.1',
+  version: '0.1.2',
   summary: 'ActiveUsers package with user tables, thumbnail page, basic profiles, etc.',
   git: 'https://github.com/clinical-meteor/clinical-active-users',
   documentation: 'README.md',
